ad84c8ccef0d0a698cc2eea377fdfdba886a0f97
akansha-singh <singhakansha281@gmail.com>
2020-06-23 14:12:55 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import MenuComponent from './components/MenuComponent';
+// import {Navbar,NavbarBrand} from 'reactstrap';
+// import './App.css';
+// import DISHES from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar dark color="primary">
+//         <div className="container">
+//           <NavbarBrand href="/">Reatorante Con Fusionbg </NavbarBrand>
+//         </div>
+//       </Navbar>
+//       <MenuComponent/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import MenuComponent from './components/MenuComponent';
+import {Navbar,NavbarBrand} from 'reactstrap';
+import './App.css';
+import { DISHES } from './shared/dishes';
+
+
+export class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Reatorante Con Fusionbg </NavbarBrand>
+          </div>
+        </Navbar>
+        <MenuComponent dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
